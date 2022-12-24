86dd2ac4b58f24a602179a348ad55abd335a0a33
ZZCWC <85448514+losier@users.noreply.github.com>
2022-12-24 21:37:22 +0530

    added coppied option

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import wouldYouRather_text from "../json_text_files/would_you_rather.json";
 
 const Home: NextPage = () => {
   const [data, setData] = useState("Click the button to load data");
+  const [popup, setPopup] = useState(false);
 
   const randomTruth = truth_text[Math.floor(Math.random() * truth_text.length)];
   const randomDare = dare_text[Math.floor(Math.random() * dare_text.length)];
@@ -43,26 +44,33 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.main_content}>
-          <code className={styles.code}>
-            {data}{" "}
-            <a
-              onClick={() => {
-                navigator.clipboard.writeText(data);
-              }}
+          <code className={styles.code}>{data} </code>
+          <a
+            className={styles.copyBtn}
+            onClick={() => {
+              navigator.clipboard.writeText(data);
+
+              setPopup(true);
+              setTimeout(() => {
+                setPopup(false);
+              }, 1500);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-clipboard"
+              viewBox="0 0 16 16"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-clipboard"
-                viewBox="0 0 16 16"
-              >
-                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-              </svg>
-            </a>
-          </code>
+              <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+              <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+            </svg>
+          </a>
+          <span className={popup ? styles.show : styles.hidden}>
+            <h2 className={styles.copiedText}>Copied</h2>
+          </span>
         </p>
         <div className={styles.grid}>
           <a
