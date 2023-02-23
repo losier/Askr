be0a4b312929ad872e8a11e60bfaa4216e56079f
losier <arv95464@gmail.com>
2023-02-23 22:35:50 +0530

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import React from "react";
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
     <>
-      <footer>losier</footer>
+      <footer className={styles.footer}>
+        <small>
+          All right reserved to{" "}
+          <Link
+            className={styles.text}
+            href="https://github.com/losier"
+            target="_blank"
+          >
+            losier <sup>&#169;</sup>
+          </Link>
+        </small>
+      </footer>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,8 +24,8 @@ export default function Home() {
       >
         <Navbar />
         <Body />
-        <Footer />
       </main>
+      <Footer />
     </>
   );
 }
