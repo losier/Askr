cbe674a18cb6b403cd3226a188fcc56467c9031d
losier <arv95464@gmail.com>
2023-02-24 17:03:32 +0530

    added never have i ever

--- src/pages/api/nhie.ts
+++ src/pages/api/nhie.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import NHIEJsonData from "../../assets/never_have_i_ever.json";
 
 type Data = {
+  id: number;
   text: string;
 };
 
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ text: "NHIE" });
+  const randomIndex = Math.floor(Math.random() * NHIEJsonData.length);
+  const randomNHIE = NHIEJsonData[randomIndex];
+  res.status(200).json({ id: randomIndex, text: randomNHIE });
 }
