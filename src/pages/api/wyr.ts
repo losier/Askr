1f6c5e9367760862dbf1da2f099a40dec536dda8
losier <arv95464@gmail.com>
2023-02-24 17:07:22 +0530

    added wyr

--- src/pages/api/wyr.ts
+++ src/pages/api/wyr.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import wyrJsonData from "../../assets/would_you_rather.json";
 
 type Data = {
+  id: number;
   text: string;
 };
 
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ text: "Would You Rather" });
+  const randomIndex = Math.floor(Math.random() * wyrJsonData.length);
+  const randomWYR = wyrJsonData[randomIndex];
+  res.status(200).json({ id: randomIndex, text: randomWYR });
 }
