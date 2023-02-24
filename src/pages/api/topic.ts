f3e57494803c0900856a0e3f3c31906357b88e71
losier <arv95464@gmail.com>
2023-02-24 17:06:24 +0530

    added topic

--- src/pages/api/topic.ts
+++ src/pages/api/topic.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import topicJsonData from "../../assets/truth.json";
 
 type Data = {
+  id: number;
   text: string;
 };
 
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ text: "Topic" });
+  const randomIndex = Math.floor(Math.random() * topicJsonData.length);
+  const randomTopic = topicJsonData[randomIndex];
+  res.status(200).json({ id: randomIndex, text: randomTopic });
 }
