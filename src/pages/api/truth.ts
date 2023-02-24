bb4c1ead5f6e2870ef184d9b9818dd66ad9a8c66
losier <arv95464@gmail.com>
2023-02-24 17:00:01 +0530

    added truth

--- src/pages/api/truth.ts
+++ src/pages/api/truth.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import truthJsonData from "../../assets/truth.json";
 
 type Data = {
+  id: number;
   text: string;
 };
 
@@ -9,5 +11,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ text: "Truth" });
+  const randomIndex = Math.floor(Math.random() * truthJsonData.length);
+  const randomTruth = truthJsonData[randomIndex];
+  res.status(200).json({ id: randomIndex, text: randomTruth });
 }
